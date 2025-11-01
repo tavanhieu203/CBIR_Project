753cd93b3890cd6c418391f87bce17f31d8766d1
wibuchuathichIT <phankhai2605@gmail.com>
2025-11-01 11:07:40 +0700

    Lưu thông tin ảnh và vector đặc trưng tương ứng

---
+++ CBIR_Project/Models/ImageFeature.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBIR_Project.Models
+{
+    public class ImageFeature //Lưu thông tin ảnh và vector đặc trưng tương ứng
+    {
+        public string ImagePath { get; set; }
+        public float[] FeatureVector { get; set; }
+
+        public ImageFeature(string path, float[] vector)
+        {
+            ImagePath = path;
+            FeatureVector = vector;
+        }
+    }
+}
