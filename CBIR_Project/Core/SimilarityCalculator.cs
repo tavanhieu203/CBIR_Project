3b9bc5156ed8e03fe73460843ffa9cba4856aed4
wibuchuathichIT <phankhai2605@gmail.com>
2025-11-01 11:05:46 +0700

    Tính độ giống giữa ảnh truy vấn và ảnh trong dataset (Euclidean, Cosine)

--- CBIR_Project/Core/SimilarityCalculator.cs
+++ CBIR_Project/Core/SimilarityCalculator.cs
@@ -8,5 +8,32 @@ namespace CBIR_Project.Core
 {
     class SimilarityCalculator
     {
+        public static double Euclidean(float[] a, float[] b)
+        {
+            double sum = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                double diff = a[i] - b[i];
+                sum += diff * diff;
+            }
+            return Math.Sqrt(sum);
+        }
+
+
+        public static double Cosine(float[] a, float[] b)
+        {
+            double dot = 0;
+            double normA = 0;
+            double normB = 0;
+
+            for (int i = 0; i < a.Length; i++)
+            {
+                dot += a[i] * b[i];
+                normA += a[i] * a[i];
+                normB += b[i] * b[i];
+            }
+
+            return dot / (Math.Sqrt(normA) * Math.Sqrt(normB));
+        }
     }
 }
